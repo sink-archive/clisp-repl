5fc0835d669205fda24806c11f0283e4be8244f8
Cain Atkinson <yellowsink@riseup.net>
2022-02-23 11:24:56 +0000

    it almost sorta works a bit woa

--- src/stdout.ts
+++ src/stdout.ts
@@ -27,50 +27,74 @@ export class PromptOutput {
 
   postProcess;
 
-  // seeks *backwards* from the end to make my life easier
-  #seekBack = 0;
+  // cursor position - in [y, x] just to annoy people :)
+  #seek = [0, 0];
 
-  #spliceIntoBuffer(i: number, c: string) {
+  get #rawSeek() {
+    const lineLengths = this.#buffer.split("\n").map((line) => line.length);
+
+    let i = this.#seek[1];
+    for (let j = 0; j < this.#seek[0]; j++) i += lineLengths[j] + 1;
+    return i;
+  }
+
+  #spliceIntoBuffer(c: string) {
+    const i = this.#rawSeek;
+
     const before = this.#buffer.slice(0, i);
     const after = this.#buffer.slice(i);
     this.#buffer = before + c + after;
   }
 
+  #goback() {
+    const lineLengths = this.#buffer.split("\n").map((line) => line.length);
+    if (this.#seek[1] > 0) this.#seek[1]--;
+    else if (this.#seek[0] > 0) {
+      this.#seek[0]--;
+      this.#seek[1] = lineLengths[this.#seek[0]];
+    }
+  }
+
+  #gofwd() {
+    const lineLengths = this.#buffer.split("\n").map((line) => line.length);
+    if (this.#seek[1] < lineLengths[this.#seek[0]]) this.#seek[1]++;
+    else if (this.#seek[0] < lineLengths.length) {
+      this.#seek[0]++;
+      this.#seek[1] = 0;
+    }
+  }
+
   writeChar(c: string) {
     if (c.length !== 1) throw new Error("c must be a single character");
     if (c === DELETE_CHAR) this.back(1);
-    else this.#spliceIntoBuffer(this.#buffer.length - this.#seekBack, this.preProcess?.(c));
+    else {
+      this.#spliceIntoBuffer(this.preProcess?.(c));
 
-    while (this.#buffer.endsWith(CURBACK(1))) {
-      const split = this.#buffer.split("\n")
-      let lI = split.length - 1;
-      let _s = this.#seekBack;
-      for (; lI >= 0; lI--) {
-        if (_s > split[lI].length) 
-          _s -= split[lI].length;
+      const bufSlice = () => {
+        let i = this.#rawSeek;
+        return this.#buffer.slice(i - 2, i + 1);
       }
-      if (_s < 0)
-        this.#seekBack++;
-      this.#buffer = this.#buffer.slice(0, -3);
-    }
 
-    while (this.#buffer.endsWith(CURFWD(1))) {
-      if (this.#seekBack > 0)
-        this.#seekBack--;
-      this.#buffer = this.#buffer.slice(0, -3);
+      while (bufSlice() === CURBACK(1)) {
+        this.back(3, false);
+        this.#goback();
+      }
+
+      while (bufSlice() === CURFWD(1)) {
+        this.back(3, false);
+        this.#gofwd();
+      }
+
+      this.#gofwd();
     }
 
     this.#display();
   }
 
-  /* remove() {
-    this.#wipeLast();
-    this.#buffer = "";
-  } */
-
-  back(n: number) {
+  back(n: number, display = true) {
     this.#buffer = this.#buffer.slice(0, -n);
-    this.#display();
+    for (let i = 0; i < n; i++) this.#goback();
+    if (display) this.#display();
   }
 
   #wipeLast() {
@@ -90,7 +114,7 @@ export class PromptOutput {
 
     this.#wipeLast();
 
-    stdout.write(toWrite) + CURBACK(this.#seekBack);
+    stdout.write(toWrite) /*  + CURBACK(this.#seekBack[1]) */;
     this.#lastDisplayLength = toWrite.split("\n").map((line) => line.length);
   }
 }
